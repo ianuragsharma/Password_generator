a6cac15ca9390510ad7062d21592046b1afecf99
ianuragsharma <41025915+ianuragsharma@users.noreply.github.com>
2021-03-21 00:15:22 +0530

    Update PaswordGen.py

--- PaswordGen.py
+++ PaswordGen.py
@@ -7,15 +7,13 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the Password Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
 
-# Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 password=""
-
+# Order not randomised:
 
 for item in range(0,nr_letters):
   rletter=random.randint(0,51)
@@ -25,14 +23,14 @@ for item in range(0,nr_symbols):
   password=symbols[rnum]+password
 for item in range(0,nr_numbers):
   rsymbol=random.randint(0,8)
-  password+=symbols[rsymbol]
-print(password)
+  password+=numbers[rsymbol]
+# print(password)
 
 
 
 #Order of characters randomised:
+
 l = list(password)
 random.shuffle(l)
 result = ''.join(l)
 print(result)
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
